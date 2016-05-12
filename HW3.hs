ebc4ea4fc7910eddf1be9350f78411a846b79457
bensaei <behnam.saeidi@gmail.com>
2016-05-12 10:03:50 -0700

    Removing unnecessary code and commenting

--- HW3.hs
+++ HW3.hs
@@ -68,9 +68,9 @@ rankP list = rank list 0
 type RnkType = Prog -> Rank -> Maybe Rank
 rank :: RnkType
 rank []     x | x >= 0            = Just x
-rank (fst:list) r | under >= 0 = rank list (under+adds)
+rank (fst:list) x | under >= 0 = rank list (under+adds)
               where (subs, adds) = rankC fst
-                    under        = r - subs
+                    under        = x - subs
 					{-
 rank (fst:list) x | eq >= 0       = rank list (eq+additions)
                                        where (subtracts, additions) = rankC fst
